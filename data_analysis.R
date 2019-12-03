9d15ce2c2046f8c36a67f66cdc76747da97fe3c7
Elizabeth G. Schmitz <egs27@pitt.edu>
2019-12-03 02:16:41 -0500

    final code before I remove outliers for fun

--- data_analysis.R
+++ data_analysis.R
@@ -8,17 +8,16 @@ raw_data<-read.csv("rawdata_blimpvsAVPI.csv")
 
 head(raw_data) ## shows initial Data Table 
 ##Make initial plot of data
-plot(AVPI~ Blimp.1, data = raw_data, pch=16, xlab= "Blimp-1 expression", ylab = "cell death",main="Cell Death vs Blimp-1 Expression")## make scatter plot 
+##plot(AVPI~ Blimp.1, data = raw_data, pch=16,col="blue", xlab= "Blimp-1 expression (portion of cells)", ylab = "cell death (portion of cells)",main="Cell Death vs Blimp-1 Expression")## make scatter plot 
 linearregression<- lm(AVPI ~ Blimp.1, data = raw_data)
 ## linear regression 
 ttestsummary<-summary(linearregression)
-sink("Results_Table.txt")
-ttestsummary
-sink()
+out<- capture.output(ttestsummary)
+cat("Results of Linear Regression testing the slope against a slope of zero",out,file ="Resultsfile.txt",sep="\n",append=FALSE)
+pdf("DataPlot.pdf")
+plot(AVPI~ Blimp.1, data = raw_data, pch=16,col="blue", xlab= "Blimp-1 expression (portion of cells)", ylab = "cell death (portion of cells)",main="Cell Death vs Blimp-1 Expression")
 abline(linearregression) ## Makes regression line
-predict(linearregression, data.frame(Blimp.1 = 0.50), se.fit = TRUE)
-anova(linearregression)
-summary(lionRegression)
+dev.off()
 
-confint(linearregression)
+
 
