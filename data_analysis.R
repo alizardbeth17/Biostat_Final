a62e56525244207600c3d87f0ce646bc10752e8a
Elizabeth G. Schmitz <egs27@pitt.edu>
2019-12-02 19:39:50 -0500

    altered graph changed variable names

--- data_analysis.R
+++ data_analysis.R
@@ -1,27 +1,24 @@
 ####Data Analysis for final Biostatistics project 
 ##path for Data C:/Users/User/Desktop/Biostatistics/Final_Project
+install.packages("devtools")
+library(devtools)
+
 
 raw_data<-read.csv("rawdata_blimpvsAVPI.csv")
-head(raw_data)
+
+head(raw_data) ## shows initial Data Table 
 ##Make initial plot of data
-plot(AVPI~ Blimp.1, data = raw_data)
+plot(AVPI~ Blimp.1, data = raw_data, pch=16, xlab= "Blimp-1 expression", ylab = "cell death",main="Cell Death vs Blimp-1 Expression")## make scatter plot 
 linearregression<- lm(AVPI ~ Blimp.1, data = raw_data)
+## linear regression 
+ttestsummary<-summary(linearregression)
+
+abline(linearregression) ## Makes regression line
+##exportablesummary<-tidyr(linearregression)
+
+predict(linearregression, data.frame(Blimp.1 = 0.50), se.fit = TRUE)
+anova(linearregression)
 summary(lionRegression)
-abline(lionRegression)
-predict(lionRegression, data.frame(Blimp.1 = 0.50), se.fit = TRUE)
-anova(lionRegression)
-summary(lionRegression)
-confint(lionRegression)
-xpt <- seq(min(lion$proportionBlack), max(lion$proportionBlack), 
-           length.out = 100)
-ypt <- data.frame( predict(lionRegression, 
-                           newdata = data.frame(proportionBlack = xpt), 
-                           interval = "confidence") )
-lines(ypt$lwr ~ xpt, lwd = 1.5, lty = 2)
-lines(ypt$upr ~ xpt, lwd = 1.5, lty = 2)
-xpt <- seq(min(lion$proportionBlack), max(lion$proportionBlack), 
-           length.out = 100)
-ypt <- data.frame(predict(lionRegression, newdata = data.frame(proportionBlack = xpt), 
-                          interval = "prediction", level = 0.95))
-lines(ypt$lwr ~ xpt, lwd = 1.5, lty = 3)
-lines(ypt$upr ~ xpt, lwd = 1.5, lty = 3)
+
+confint(linearregression)
+
