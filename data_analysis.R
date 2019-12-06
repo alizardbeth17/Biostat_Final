729768731cce0f06bdc2854d2199801d8ddd4f89
Elizabeth G. Schmitz <egs27@pitt.edu>
2019-12-06 12:14:08 -0500

    final analysis

--- data_analysis.R
+++ data_analysis.R
@@ -19,5 +19,19 @@ plot(AVPI~ Blimp.1, data = raw_data, pch=16,col="blue", xlab= "Blimp-1 expressio
 abline(linearregression) ## Makes regression line
 dev.off()
 
+## Same code now with outliers removed 
 
+raw_data<-read.csv("rawdata_nooutliers.csv")
 
+head(raw_data) ## shows initial Data Table 
+##Make initial plot of data
+##plot(AVPI~ Blimp.1, data = raw_data, pch=16,col="red", xlab= "Blimp-1 expression (portion of cells)", ylab = "cell death (portion of cells)",main="Cell Death vs Blimp-1 Expression")## make scatter plot 
+linearregression<- lm(AVPI ~ Blimp.1, data = raw_data)
+## linear regression 
+ttestsummary<-summary(linearregression)
+out<- capture.output(ttestsummary)
+cat("Results of Linear Regression testing the slope against a slope of zero",out,file ="Resultsfile_nooutliers.txt",sep="\n",append=FALSE)
+pdf("DataPlot_nooutliers.pdf")
+plot(AVPI~ Blimp.1, data = raw_data, pch=16,col="blue", xlab= "Blimp-1 expression (portion of cells)", ylab = "cell death (portion of cells)",main="Cell Death vs Blimp-1 Expression")
+abline(linearregression) ## Makes regression line
+dev.off()
